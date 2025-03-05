e220bcd5b24da158983c23068b1fcbd54fdfd458
Andres R <andres@uma.es>
2025-03-05 23:12:46 +0000

    trabajando

---
+++ cuenta_freq.c
@@ -0,0 +1,103 @@
+#include <omp.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+
+char *leer_fasta(const char *nombre_archivo, int *size) {
+    FILE *archivo = fopen(nombre_archivo, "r");
+    if (!archivo) {
+        perror("Error abriendo el archivo");
+        return NULL;
+    }
+
+    char linea[1024];  
+    fgets(linea, sizeof(linea), archivo);  // Leer y descartar la cabecera
+
+    // Calcular el tamaño de la secuencia (ignorando saltos de línea)
+    int tamaño = 0;
+    while (fgets(linea, sizeof(linea), archivo)) {
+        for (int i = 0; linea[i] != '\0'; i++) {
+            if (isalpha(linea[i])) tamaño++;  // Contar solo letras (A, C, G, T)
+        }
+    }
+    *size=tamaño;
+    // Reservar memoria para la secuencia
+    char *secuencia = (char *)malloc(tamaño + 1);
+    if (!secuencia) {
+        perror("Error al reservar memoria");
+        fclose(archivo);
+        return NULL;
+    }
+
+    // Reiniciar el archivo para leer la secuencia
+    rewind(archivo);
+    fgets(linea, sizeof(linea), archivo);  // Saltar la cabecera nuevamente
+
+    // Leer la secuencia
+    size_t pos = 0;
+    while (fgets(linea, sizeof(linea), archivo)) {
+        for (int i = 0; linea[i] != '\0'; i++) {
+            if (isalpha(linea[i])) {
+                secuencia[pos++] = linea[i];
+            }
+        }
+    }
+    secuencia[pos] = '\0';  // Finalizar la cadena
+    fclose(archivo);
+    return secuencia;
+}
+
+
+int main(int argc, char *argv[])
+{
+  char host[256];
+  int frequencia0,frequencia1,frequencia2,frequencia3; // ACGT
+  int tamaño=0;
+  int i;
+  
+  gethostname(host,256);  // unistd.h
+  printf("Hello world! ejecutando en %s\n", host);
+  printf("Ejecutando con un solo hilo\n");
+
+  if (argc != 2) {
+        fprintf(stderr, "Uso: %s <archivo_fasta>\n", argv[0]);
+        return 1;
+   }
+  printf(" > Cargando fichero: %s\n", argv[1]);
+
+  char *secuencia = leer_fasta(argv[1], &tamaño);
+  if (!secuencia) {
+      return 1;
+  }
+
+  printf(" > Tamaño secuencia %d\n", tamaño);
+
+  frequencia0=0;
+  frequencia1=0;
+  frequencia2=0;
+  frequencia3=0;
+
+  for(i=0;i<tamaño;i++)
+  {
+   switch(secuencia[i])
+   {
+    case 'A': frequencia0++; break; 
+    case 'C': frequencia1++; break;
+    case 'G': frequencia2++; break;
+    case 'T': frequencia3++; break;
+   }  
+  }
+
+  printf("  Frequencia  A : %d - %f %%\n",frequencia0,(float)frequencia0/tamaño*100); 
+  printf("  Frequencia  C : %d - %f %%\n",frequencia1,(float)frequencia1/tamaño*100); 
+  printf("  Frequencia  G : %d - %f %%\n",frequencia2,(float)frequencia2/tamaño*100); 
+  printf("  Frequencia  T : %d - %f %%\n",frequencia3,(float)frequencia3/tamaño*100); 
+  
+
+  free(secuencia);  // Liberar memoria
+
+  return 0;
+}
+
--- cuenta_freq_omp.c
+++ cuenta_freq_omp.c
@@ -2,41 +2,78 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 
-int 
-main(int argc, char *argv[])
+char *leer_fasta(const char *nombre_archivo, int *size) {
+    FILE *archivo = fopen(nombre_archivo, "r");
+    if (!archivo) {
+        perror("Error abriendo el archivo");
+        return NULL;
+    }
+
+    char linea[1024];  
+    fgets(linea, sizeof(linea), archivo);  // Leer y descartar la cabecera
+
+    // Calcular el tamaño de la secuencia (ignorando saltos de línea)
+    int tamaño = 0;
+    while (fgets(linea, sizeof(linea), archivo)) {
+        for (int i = 0; linea[i] != '\0'; i++) {
+            if (isalpha(linea[i])) tamaño++;  // Contar solo letras (A, C, G, T)
+        }
+    }
+    *size=tamaño;
+    // Reservar memoria para la secuencia
+    char *secuencia = (char *)malloc(tamaño + 1);
+    if (!secuencia) {
+        perror("Error al reservar memoria");
+        fclose(archivo);
+        return NULL;
+    }
+
+    // Reiniciar el archivo para leer la secuencia
+    rewind(archivo);
+    fgets(linea, sizeof(linea), archivo);  // Saltar la cabecera nuevamente
+
+    // Leer la secuencia
+    size_t pos = 0;
+    while (fgets(linea, sizeof(linea), archivo)) {
+        for (int i = 0; linea[i] != '\0'; i++) {
+            if (isalpha(linea[i])) {
+                secuencia[pos++] = linea[i];
+            }
+        }
+    }
+    secuencia[pos] = '\0';  // Finalizar la cadena
+    fclose(archivo);
+    return secuencia;
+}
+
+
+int main(int argc, char *argv[])
 {
   char host[256];
   int frequencia0,frequencia1,frequencia2,frequencia3; // ACGT
-  int seqsize;
+  int tamaño=0;
   int i;
-  char *seq;
-  FILE *f;
-
-
-
+  
   gethostname(host,256);  // unistd.h
-  printf("Hello world! I am  at %s\n", host);
+  printf("Hello world! ejecutando en %s\n", host);
+  printf("Número máximo de hilos disponibles: %d\n", omp_get_max_threads());
 
-     if(argc!=2) 
-     { 
-       perror("No file selected. Use program: <filename>\n");
-       exit(-1);
-     }
-     printf(" > Loading FILE %s \n", argv[1]);
 
-     if((f=fopen(argv[1],"rt"))==NULL) 
-     { 
-       perror("Loading sequence File\n");
-       exit(-1);
-     }
-     fscanf(f," %d ", &seqsize);
-     printf(" > File size %d\n", seqsize);
-     seq=(char*) malloc(sizeof(char)*seqsize);  
-     for(i=0;i<seqsize;i++) fscanf(f," %c ",seq+i);
-    
+  if (argc != 2) {
+        fprintf(stderr, "Uso: %s <archivo_fasta>\n", argv[0]);
+        return 1;
+   }
+  printf(" > Cargando fichero: %s\n", argv[1]);
 
+  char *secuencia = leer_fasta(argv[1], &tamaño);
+  if (!secuencia) {
+      return 1;
+  }
+
+  printf(" > Tamaño secuencia %d\n", tamaño);
 
   frequencia0=0;
   frequencia1=0;
@@ -45,24 +82,27 @@ main(int argc, char *argv[])
 
   omp_set_num_threads(4);	
 
-#pragma omp parallel for private(i) shared(seq,seqsize) \
+#pragma omp parallel for private(i) shared(secuencia,tamaño) \
         reduction(+:frequencia0,frequencia1,frequencia2,frequencia3)
-  for(i=0;i<seqsize;i++)
+  for(i=0;i<tamaño;i++)
   {
-   switch(seq[i])
+   switch(secuencia[i])
    {
-    case 'a': frequencia0++; break; 
-    case 'c': frequencia1++; break;
-    case 'g': frequencia2++; break;
-    case 't': frequencia3++; break;
+    case 'A': frequencia0++; break; 
+    case 'C': frequencia1++; break;
+    case 'G': frequencia2++; break;
+    case 'T': frequencia3++; break;
    }  
   }
 
-     printf("  Frequencia  A : %d - %f %%\n",frequencia0,(float)frequencia0/seqsize*100); 
-     printf("  Frequencia  C : %d - %f %%\n",frequencia1,(float)frequencia1/seqsize*100); 
-     printf("  Frequencia  G : %d - %f %%\n",frequencia2,(float)frequencia2/seqsize*100); 
-     printf("  Frequencia  T : %d - %f %%\n",frequencia3,(float)frequencia3/seqsize*100); 
-     
+  printf("  Frequencia  A : %d - %f %%\n",frequencia0,(float)frequencia0/tamaño*100); 
+  printf("  Frequencia  C : %d - %f %%\n",frequencia1,(float)frequencia1/tamaño*100); 
+  printf("  Frequencia  G : %d - %f %%\n",frequencia2,(float)frequencia2/tamaño*100); 
+  printf("  Frequencia  T : %d - %f %%\n",frequencia3,(float)frequencia3/tamaño*100); 
+  
+
+  free(secuencia);  // Liberar memoria
+
   return 0;
 }
 
